f0a9452508c0114d312f83782dd125baa3f18bce
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-11 20:47:14 +0530

    Create Abundant_Numbers.c

---
+++ Abundant_Numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,i,c=0;
+  scanf("%d",&a);
+  for(i=1;i<a;i++)
+  {
+     if(a%i==0)
+       c=c+i;
+  }
+  if(a<C)
+  {
+    printf("True");
+  }
+  else
+  {
+     printf("False");
+  }
+}
+
