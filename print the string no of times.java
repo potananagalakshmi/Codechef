ca4bf94031696c2a4ea862dd54e8083381ac943a
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-18 12:13:11 +0530

    Create print the string no of times.java

---
+++ print the string no of times.java
@@ -0,0 +1,9 @@
+public class loops {
+    public static void main(String[] args){
+        int i=1;//Intialization
+        while(i<=10){//condition
+            System.out.println("Naga");
+            i++;//updation
+        }
+    }  
+}
