417d30e61a306ffc93a22c12240b134102d7d0d2
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-25 10:56:04 +0530

    Create add two numbers in range.java

---
+++ add two numbers in range.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class  naga{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a,b,c,d;
+        a=s.nextInt();
+        b=s.nextInt();
+        c=s.nextInt();
+        d=b+c;
+        for(int i=0;i<=a;i++)
+        {
+            System.out.print(d);
+        }
+    }
+}
