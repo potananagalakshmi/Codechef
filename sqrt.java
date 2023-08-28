24edfc7f11dc4e0caff0086318be60040b5effa1
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-28 22:00:10 +0530

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,9 @@
+ class sqrt {
+    public static void main(String[] args){
+        int intVar=25;
+        // double squareRoot =Math.sqrt(intVar);//sqrt is will be in double
+        //if we want to write sqrt in int
+        int squareRoot=(int)Math.sqrt(intVar);
+        System.out.println(squareRoot);
+    }
+}
