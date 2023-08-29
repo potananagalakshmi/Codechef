99af0b13b06d7338bdfd90d252c4fd1eb54bc50c
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-29 22:19:42 +0530

    Create if& else problem.java

---
+++ if& else problem.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int Age = 25;
+        int Vage = 18;
+
+        if (Age >= Vage) {
+            System.out.println("Old enough to vote!");
+        } else {
+            System.out.println("Not old enough to vote.");
+        }
+	}
+}
