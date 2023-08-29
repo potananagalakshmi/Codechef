516f6023e210d13234a51af80ec12d90a6cb06c7
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-29 22:18:00 +0530

    Create area and perimeter of rectangle.java

---
+++ area and perimeter of rectangle.java
@@ -0,0 +1,11 @@
+class CodeChef
+{
+	public static void main (String[] args)
+	{
+	    // area - length * breadth 
+		System.out.println(11*13);
+		
+		//perimeter - 2(length + breadth)
+		System.out.print(2 * (11+13));
+	}
+}
