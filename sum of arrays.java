d8b48b70a6bf76a6523fc3778c28bdf1d534e2c5
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-08-29 22:14:00 +0530

    Create sum of arrays.java

---
+++ sum of arrays.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class sum{
+  public static void main(String[] args){
+    int r;
+    r=sc.nextInt();
+    int[] arr=new int[r];
+    for(int i=0;i<r;i++){
+      arr[i]=sc.nextInt();
+    }
+    int sum=0;
+    for(int num=sum){
+        sum+=num;
+    }
+    System.out.print(sum);
+  }
+}
