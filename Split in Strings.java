f011907fa6c9d0491d2dc6e19d5a19b544372464
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-03 22:44:02 +0530

    Create Split in Strings.java

---
+++ Split in Strings.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class String8 {
+    public static void main(String[] args){
+        String s="This is naga Lakshmi";
+        String[] arr=s.split(" ");
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
