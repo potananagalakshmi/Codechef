a192683951bd4f37a546cea16f644bcbc77cedf6
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-03 22:40:16 +0530

    Create String Concept 4.java

---
+++ String Concept 4.java
@@ -0,0 +1,11 @@
+public class String4 {
+    public static void main(String[] args){
+        String s="";
+        s+='a';
+        s+='b';
+        s+='c';
+        s+='d';
+        System.out.println(s);
+    }
+    
+}
