7a6b5efbb5aa83b0be628d0c0f3f50f733683adc
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-03 22:43:24 +0530

    Create Compare in the String.java

---
+++ Compare in the String.java
@@ -0,0 +1,11 @@
+public class String7 {
+    public static void main(String[] args) {
+        
+        String s1="Arav";
+        String s2="arab";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s2.compareTo(s1));
+
+    }
+    
+}
