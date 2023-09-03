503e4e28e7c44616066cf4b2db6e154e4d207d4b
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-03 22:44:41 +0530

    Create Contains in string.java

---
+++ Contains in string.java
@@ -0,0 +1,11 @@
+public class String9 {
+    public static void main(String[] args){
+        String s1="Hello World";
+        String s2="llo";
+        String s3="llw";
+        System.out.println(s1.contains(s2));
+        System.out.println(s1.contains(s3));
+
+    }
+    
+}
