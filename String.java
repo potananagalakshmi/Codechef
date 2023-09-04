348a92b54083a88c0dc507663ee17b3f743da64d
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2023-09-04 22:17:34 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,11 @@
+public class Strings2 {
+    public static void main(String[] args){
+        String s="Hello world";
+        // for(int i=0;i<s.length();i++){
+        //     System.out.println(s.charAt(i));
+        // }
+        for(char e:s.toCharArray()){
+        System.out.println(e);
+    }
+}
+}
