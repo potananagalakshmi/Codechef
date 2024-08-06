da98a053fe442c3be9ae38ba527b209d27cbe0cd
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2024-08-06 22:19:22 +0530

    Create clear day.java

---
+++ clear day.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class naga{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        //int c=(7-a);
+        //int d=(c-b);
+        System.out.println(7-a-b);
+    }
+}
