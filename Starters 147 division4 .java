17181bd3f3c9fd8e57381fe61648cfe5e80f36c7
potananagalakshmi <119916338+potananagalakshmi@users.noreply.github.com>
2024-08-14 21:22:26 +0530

    Create Starters 147 division4 .java

---
+++ Starters 147 division4 .java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		if(c>d)
+		{
+		    System.out.println(a);
+		}
+		else
+		{
+		    System.out.println(b);
+		}
+
+	}
+}
